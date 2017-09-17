7daa0874cb100aa799018cfcc29f7274167da8dc
takaaki.konaka <takaaki.konaka@gaiax.com>
2017-09-17 19:25:46 +0900

    やり取りするくんのテスト

--- php/test/CellMediator.php
+++ php/test/CellMediator.php
@@ -9,12 +9,18 @@ class CellMediatorTest extends PHPUnit_Framework_TestCase
     {
         $cell_creator = new CellCreator(2, 2);
         $cell_mediators = new CellMediators(2, 2, $cell_creator->initialize());
+        $cell_mediators = $cell_mediators->initialize();
 
-        foreach($cell_mediators->initialize() as $cell_mediator)
-        {
-            echo('---------------------------------');
-            var_dump($cell_mediator);
-        }
+
+        $cell_mediator = $cell_mediators[0];
+        $this->assertInstanceOf('NullCell', $cell_mediator->arround_cells[0]);
+        $this->assertInstanceOf('NullCell', $cell_mediator->arround_cells[1]);
+        $this->assertInstanceOf('NullCell', $cell_mediator->arround_cells[2]);
+        $this->assertInstanceOf('NullCell', $cell_mediator->arround_cells[3]);
+        $this->assertInstanceOf('Cell', $cell_mediator->arround_cells[4]);
+        $this->assertInstanceOf('NullCell', $cell_mediator->arround_cells[5]);
+        $this->assertInstanceOf('Cell', $cell_mediator->arround_cells[6]);
+        $this->assertInstanceOf('Cell', $cell_mediator->arround_cells[7]);
     }
 }
 
