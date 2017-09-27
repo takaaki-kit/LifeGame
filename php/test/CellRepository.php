f23db9ab6974fd80a80d34ef6bcaf4bb1ba1189c
Takaaaki_z <takaaki.kit@gmail.com>
2017-09-27 18:16:16 +0900

    一般化のために用意したテストは不要なので捨てる

--- php/test/CellRepository.php
+++ php/test/CellRepository.php
@@ -13,14 +13,4 @@ class CellRepositoryTest extends PHPUnit_Framework_TestCase
 		$expect = array(new Cell(1, 1));
 		$this->assertEquals($expect, $cell_repository->all());
 	}
-
-	public function test_aセルを追加する()
-	{
-		$cell_repository = new CellRepository();
-		$cell_repository->add(new Cell(1, 1));
-		$cell_repository->add(new Cell(1, 2));
-
-		$expect = array(new Cell(1, 1), new Cell(1, 2));
-		$this->assertEquals($expect, $cell_repository->all());
-	}
 }
